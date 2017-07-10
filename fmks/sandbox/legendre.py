e198905266d4ca744d19823432b8544a2b076302
auag92 <apaar92@gmail.com>
2017-07-10 14:11:09 -0400

    both legendre and primitive bases cleaned

--- fmks/sandbox/legendre.py
+++ fmks/sandbox/legendre.py
@@ -1,33 +1,44 @@
-import fmks
 import numpy as np
 import numpy.polynomial.legendre as leg
+
 
 def is_in_domain(X, domain):
     return ((np.min(X) < domain[0]) or (np.max(X) > domain[1]))
 
+
 def scaled_x(X, domain):
     return ((2.*X-domain[0]-domain[1])/(domain[1]-domain[0]))
+
 
 def norm(n_states):
     return (2.*np.array(n_states)+1)/2.
 
+
 def coeff(n_states):
     return np.eye(len(n_states))*norm(n_states)
 
+
 def leg_x(X, domain, n_states):
-     return (leg.legval(scaled_x(X, domain),coeff(n_states)))
+     return (leg.legval(scaled_x(X, domain), coeff(n_states)))
+    
+
+def rollaxis_(X):
+    return np.rollaxis(X, 0, len(X.shape))
+
 
 def discretize(X, domain=[-1,1], n_states=np.arange(2)):
-        X_Legendre = leg_x(X, domain, n_states)
-        return np.rollaxis(X_Legendre, 0, len(X_Legendre.shape))
+        return rollaxis_(leg_x(X, domain, n_states))
+    
 
 def legendre_basis(X, domain=[-1,1], n_states=2):
     if not is_in_domain(X, domain):
         if isinstance(n_states, int):
-            n_states = np.arange(n_states)
-        return discretize(X, domain, n_states)
+            return discretize(X, domain, np.arange(n_states))
+        else:
+            return discretize(X, domain, n_states)
     else:
         raise RuntimeError("X must be within the specified domain")
+
 
 def P(x):
     x = 4 * x - 1
